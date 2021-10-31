b6e7809e583721a511c46fcf573d3e93d02cae53
Chris O'Neil <chriso83@protonmail.com>
2021-10-31 01:31:37 +0000

    feat: finalise `source-port ls` command
    Finally, after all the refactoring and deliberation over the implementation of this feature, the
    command handler is updated to present the information and there's an integration test that will hit
    the Github API to make sure all the latest versions are returned (and source ports that don't have a
    latest version will be indicated).
    
    The caching part is all unit tested, so I'm confident that should be covered just by unit testing.

--- tests/source_port_ls.rs
+++ tests/source_port_ls.rs
@@ -1,13 +1,34 @@
-/// These tests are for parsing IWAD files. The only legal way to obtain IWADs are by purchasing
-/// them, so these tests shouldn't run on the public, remote CI environment, since that would involve
-/// copying the IWADs there. These tests will just need to be executed locally.
+/// Runs the `source-port ls` command.
+///
+/// This command should list the latest version for each supported source port, or, if there's no
+/// version marked as latest, that will be indicated.
+///
+/// Due to the fact that these tests hit the Github API, they need to be prevented from running as
+/// part of the normal test run, and that's why they used the `rate-limiting-tests` feature. Github
+/// are actually fairly stringent about the number of unathenticated hits on the API.
 use assert_cmd::Command;
 use predicates::prelude::*;
 
 #[cfg(feature = "rate-limiting-tests")]
 const CHOCOLATE_LATEST_VERSION: &str = "3.0.0";
 #[cfg(feature = "rate-limiting-tests")]
-const DOOM_RETRO_LATEST_VERSION: &str = "4.2.3";
+const CRISPY_LATEST_VERSION: &str = "5.10.3";
+#[cfg(feature = "rate-limiting-tests")]
+const DOOM_RETRO_LATEST_VERSION: &str = "4.3";
+#[cfg(feature = "rate-limiting-tests")]
+const DSDA_DOOM_LATEST_VERSION: &str = "0.21.3";
+#[cfg(feature = "rate-limiting-tests")]
+const ETERNITY_ENGINE_LATEST_VERSION: &str = "4.02.00";
+#[cfg(feature = "rate-limiting-tests")]
+const GZDOOM_LATEST_VERSION: &str = "4.7.1";
+#[cfg(feature = "rate-limiting-tests")]
+const LZDOOM_LATEST_VERSION: &str = "3.88a";
+#[cfg(feature = "rate-limiting-tests")]
+const ODAMEX_LATEST_VERSION: &str = "0.9.5";
+#[cfg(feature = "rate-limiting-tests")]
+const PRBOOM_LATEST_VERSION: &str = "2.6.1um";
+#[cfg(feature = "rate-limiting-tests")]
+const WOOF_LATEST_VERSION: &str = "7.0.0";
 
 #[cfg(feature = "rate-limiting-tests")]
 #[test]
@@ -27,6 +48,32 @@ fn source_port_ls_should_list_latest_versions_for_supported_source_ports() {
                 .unwrap(),
         )
         .stdout(
+            predicate::str::is_match(format!("Crispy Doom.*{}", CRISPY_LATEST_VERSION)).unwrap(),
+        )
+        .stdout(
             predicate::str::is_match(format!("Doom Retro.*{}", DOOM_RETRO_LATEST_VERSION)).unwrap(),
-        );
+        )
+        .stdout(
+            predicate::str::is_match(format!("DSDA Doom.*{}", DSDA_DOOM_LATEST_VERSION)).unwrap(),
+        )
+        .stdout(
+            predicate::str::is_match(format!(
+                "Eternity Engine.*{}",
+                ETERNITY_ENGINE_LATEST_VERSION
+            ))
+            .unwrap(),
+        )
+        .stdout(predicate::str::is_match(format!("GZDoom.*{}", GZDOOM_LATEST_VERSION)).unwrap())
+        .stdout(predicate::str::is_match(format!("LZDoom.*{}", LZDOOM_LATEST_VERSION)).unwrap())
+        .stdout(predicate::str::is_match(format!("Odamex.*{}", ODAMEX_LATEST_VERSION)).unwrap())
+        .stdout(
+            predicate::str::is_match(format!("PrBoom Plus.*{}", PRBOOM_LATEST_VERSION)).unwrap(),
+        )
+        .stdout(predicate::str::is_match(format!("Woof.*{}", WOOF_LATEST_VERSION)).unwrap())
+        .stdout(predicate::str::contains(
+            "RUDE has no version marked as latest",
+        ))
+        .stdout(predicate::str::contains(
+            "Zandronum has no version marked as latest",
+        ));
 }
